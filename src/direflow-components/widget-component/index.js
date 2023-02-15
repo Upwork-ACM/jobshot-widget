3cad2d206a290e79990af6426d4ec9832466dfe9
Ozara Tsubasa <alcantaracloyd26@gmail.com>
2023-02-15 23:20:08 +0800

    update

--- src/direflow-components/widget-component/index.js
+++ src/direflow-components/widget-component/index.js
@@ -2,7 +2,7 @@ import { DireflowComponent } from 'direflow-component';
 import App from './App';
 
 const component = document.querySelector('widget-component');
-var type   = component.toString().split(' ');
+var type   = component;
 console.log('component', type)
 export default DireflowComponent.create({
   component: App,
