c3ba817fa550b71d740f9ba990077e4041a96df2
Ozara Tsubasa <alcantaracloyd26@gmail.com>
2023-02-15 23:00:37 +0800

    update

---
+++ src/direflow-components/widget-component/App.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+import { Styled } from 'direflow-component';
+import styles from './App.css';
+
+const App = (props) => {
+  useEffect(() => {
+    console.log(props)
+  }, [props])
+  return (
+    <Styled styles={styles}>
+      <p>TESTING</p>
+    </Styled>
+  );
+};
+
+export default App;
---
+++ src/direflow-components/widget-component/index.js
@@ -0,0 +1,21 @@
+import { DireflowComponent } from 'direflow-component';
+import App from './App';
+
+const component = document.querySelector('widget-component');
+console.log('component', component)
+export default DireflowComponent.create({
+  component: App,
+  configuration: {
+    tagname: 'widget-component'
+  },
+  plugins: [
+    {
+      name: 'font-loader',
+      options: {
+        google: {
+          families: ['Advent Pro', 'Noto Sans JP'],
+        },
+      },
+    },
+  ],
+});
