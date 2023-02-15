49ecccef51751f8b92a82c28d538d0d4d2148320
Ozara Tsubasa <alcantaracloyd26@gmail.com>
2023-02-15 23:04:29 +0800

    update

--- src/direflow-components/widget-component/index.js
+++ src/direflow-components/widget-component/index.js
@@ -2,7 +2,7 @@ import { DireflowComponent } from 'direflow-component';
 import App from './App';
 
 const component = document.querySelector('widget-component');
-console.log('component', component)
+console.log('component', component.getAttribute('id'))
 export default DireflowComponent.create({
   component: App,
   configuration: {
